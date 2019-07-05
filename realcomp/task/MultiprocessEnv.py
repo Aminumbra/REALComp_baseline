Fixed normalization : rewards are correctly normalized. Tried to use a shaped reward function to lift objects.

realcomp/task/MultiprocessEnv.py
@@ -37,6 +37,9 @@ def worker(remote, parent_remote, env_fn_wrapper):
         elif cmd == 'get_part_pos':
             part_pos = env.get_part_pos(data)
             remote.send(part_pos)
+        elif cmd == 'get_touch_sensors':
+            touch_sensors = env.robot.get_touch_sensors()
+            remote.send(touch_sensors)
         elif cmd == 'render':
             if data is not None:
                 env.render(data)
@@ -201,6 +204,7 @@ class RobotVecEnv(SubprocVecEnv):
         super(RobotVecEnv, self).__init__(env_fns)
 
         self.keys = keys
+        self.observation_space = np.array(13 + 3*3)
 
     def obs_to_array(self, obs):
         converted_obs = []
@@ -239,6 +243,11 @@ class RobotVecEnv(SubprocVecEnv):
             remote.send(('get_part_pos', part))
         return np.stack([remote.recv() for remote in self.remotes])
 
+    def get_touch_sensors(self):
+        for remote in self.remotes:
+            remote.send(('get_touch_sensors', None))
+        return np.stack([remote.recv() for remote in self.remotes])
+
     def step_wait(self):
         results = [remote.recv() for remote in self.remotes]
         self.waiting = False
@@ -263,9 +272,11 @@ class VecNormalize(RobotVecEnv):
     and returns from an environment.
     """
 
-    def __init__(self, envs, size_obs_to_norm=13, ob=True, ret=True, clipob=10., cliprew=10., gamma=0.99, epsilon=1e-8, use_tf=False):
+    def __init__(self, envs, size_obs_to_norm=13, ob=True, ret=True, clipob=10., cliprew=10., gamma=0.95, epsilon=1e-8, use_tf=False):
         self.envs = envs
         self.size_obs_to_norm = size_obs_to_norm
+
+        self.i = 0
         
         if use_tf:
             from baselines.common.running_mean_std import TfRunningMeanStd
@@ -296,7 +307,7 @@ class VecNormalize(RobotVecEnv):
             trunc_obs = obs[:, :self.size_obs_to_norm]
             self.ob_rms.update(trunc_obs)
             trunc_obs = np.clip((trunc_obs - self.ob_rms.mean) / np.sqrt(self.ob_rms.var + self.epsilon), -self.clipob, self.clipob)
-            obs[:, :self.size_obs_to_norm] = trunc_obs
+            obs[:, :self.size_obs_to_norm] = trunc_obs            
             return obs
         else:
             return obs
@@ -305,6 +316,13 @@ class VecNormalize(RobotVecEnv):
         if self.ret_rms:
             self.ret_rms.update(self.ret)
             rews = np.clip(rews / np.sqrt(self.ret_rms.var + self.epsilon), -self.cliprew, self.cliprew)
+
+            # config.tensorboard.add_scalar("normalize/ob_rms_mean", self.ob_rms.mean.mean(), self.i)
+            # config.tensorboard.add_scalar("normalize/ob_rms_var", self.ob_rms.var.mean(), self.i)
+            # config.tensorboard.add_scalar("normalize/ret_rms_mean", self.ret_rms.mean.mean(), self.i)
+            # config.tensorboard.add_scalar("normalize/ret_rms_var", self.ret_rms.var.mean(), self.i)
+            # self.i += 1
+            
             return rews
         else:
             return rews

realcomp/task/demo.py
@@ -63,7 +63,7 @@ def demo_run():
                           actor_lr=1e-4,
                           critic_lr=1e-3,
                           value_loss_coeff=1.,
-                          gamma=0.9,
+                          gamma=0.95,
                           gae_lambda=0.95,
                           epochs=10,
                           horizon=32,
@@ -141,6 +141,10 @@ def demo_run():
             # if config.save_every and frame and frame % config.save_every == 0:
             #     controller.save_models("models.pth")
 
+            # Used to reset the normalization. All the envs. terminate at the same time, so we can do this
+            if any(done):
+                envs.ret = done
+            
             if new_episode:
                 new_episode = False
                 # Add things : change current goal, etc
@@ -264,29 +268,27 @@ def get_contacts(envs, target_object, punished_objects):
 
 
 def update_reward(envs, frame, reward, some_state, target_object="orange", punished_objects=["mustard", "tomato"]):
-    obj_init_pos = np.ndarray((3, len(envs), 3))
-    obj_cur_pos = np.ndarray((3, len(envs), 3))
 
     if frame == 0:
         pass
 
-    elif frame % config.frames_per_action == 0 and frame >= config.noop_steps:
-        for i, obj in enumerate(objects_names):
-            obj_init_pos[i] = envs.get_obj_pos(obj)  # We associate to the i-th object an array of length len(envs),  whose elements are positions (3-tuples)
-
     good_contacts, bad_contacts = get_contacts(envs, target_object, punished_objects)
     robot_useful_parts = ["finger_00", "finger_01", "finger_10", "finger_11"]  # 4 fingers + the last part of the robot (~ "its hand")
+    target_pos = envs.get_obj_pos(target_object)
 
     if frame > config.noop_steps:
-        for i, obj in enumerate(objects_names):
-            obj_cur_pos[i] = envs.get_obj_pos(obj)
 
-        distance_target = np.minimum.reduce([euclidean_distance(envs.get_obj_pos(target_object), envs.get_part_pos(robot_part)) for robot_part in robot_useful_parts])
+        distance_target = np.minimum.reduce([euclidean_distance(target_pos, envs.get_part_pos(robot_part)) for robot_part in robot_useful_parts])
 
         closeness = np.power(distance_target + 1e-6, -2)
         reward = np.clip(closeness, 0, 10)
-
+        
+        # touch_sensors = envs.get_touch_sensors()
+        # sensors_activated = np.array(np.max(touch_sensors, axis=1), dtype=bool) # Array of length num_envs, containing True if one of the sensors > 0, False otherwise
+        # lift_reward = target_pos[:, 2] * sensors_activated
+        
         some_state += reward
+        #some_state += lift_reward * 400
         reward = some_state.copy()
 
         if not frame % config.frames_per_action: # Only interested in the final step of the action for the contact with the target
@@ -296,6 +298,7 @@ def update_reward(envs, frame, reward, some_state, target_object="orange", punis
     #assert frame <= config.noop_steps or reward.mean() > 0        
     reward = reward * 0.01
 
+    envs.ret = envs.ret * envs.gamma + reward
     reward = envs._rewfilt(reward)
     
     return reward, good_contacts, some_state